9e7ce6dddac3015b9be4a40959fcc1a2a4e83953
aline <Aline.souza21@outlook.com.br>
2024-12-06 09:59:37 -0300

    Jest- add

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  bail: true,
+  coverageProvider: "v8",
+  testMatch: ["<rootDie>/src/**/*.spec.js"],
+};
--- src/database/knex/migrations/20240829133038_createtags.js
+++ src/database/knex/migrations/20240829133038_createtags.js
@@ -1,4 +1,4 @@
-exports.up = (knex) =>
+Tagsexports.up = (knex) =>
   knex.schema.createTable("tags", (table) => {
     table.increments("id");
     table.text("name").notNullable();
